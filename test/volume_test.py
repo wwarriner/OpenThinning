48f9bbd5e6ed158521183a1c750ee7f0ba53451a
William Warriner <wwarriner@gmail.com>
2023-06-08 22:59:44 -0500

    cleanup tests

--- test/volume_test.py
+++ test/volume_test.py
@@ -1,52 +1,9 @@
-import functools
-import logging
-import time
 import unittest
 
 import numpy as np
 import numpy.testing as npt
 
-from src.lookup_table import LookupTable
 from src.volume import *
-
-logging.basicConfig(level=logging.INFO, filemode="w")
-
-FN_NAME = "__fn_name"
-
-
-def with_log(base_name: str = "", *dargs, **dkwargs):
-    def dec(fn):
-        # @functools.wraps(fn)
-        def _(*args, **kwargs):
-            name = fn.__name__
-            if base_name != "":
-                name = "_".join([name, base_name])
-            logging.basicConfig(
-                filename=f"{name}.log",
-                level=logging.INFO,
-                filemode="w",
-                force=True,
-                *dargs,
-                **dkwargs,
-            )
-            return fn(*args, **kwargs)
-
-        return _
-
-    return dec
-
-
-def set_timing_file(name: str) -> None:
-    logging.basicConfig(filename=f"{name}.log")
-
-
-def cube_to_neighborhood(_v: np.ndarray) -> np.ndarray:
-    arr = _v.ravel()
-    mask = np.ones(np.prod(arr.shape), dtype=np.bool_)
-    mask[13] = False
-    arr = arr[mask]
-    arr = np.expand_dims(arr, 0)
-    return arr
 
 
 class VolumeTest(unittest.TestCase):
@@ -55,12 +12,12 @@ class VolumeTest(unittest.TestCase):
 
         v = Volume.true((3, 3, 3))
         npt.assert_array_equal(
-            v.get_neighborhood(center), np.ones((1, 26), dtype=np.bool_)
+            v._get_neighborhood(center), np.ones((1, 26), dtype=np.bool_)
         )
 
         v = Volume.false((3, 3, 3))
         npt.assert_array_equal(
-            v.get_neighborhood(center), np.zeros((1, 26), dtype=np.bool_)
+            v._get_neighborhood(center), np.zeros((1, 26), dtype=np.bool_)
         )
 
         """
@@ -84,7 +41,7 @@ class VolumeTest(unittest.TestCase):
             dtype=np.bool_,
         )
         npt.assert_array_equal(
-            Volume(arr).get_neighborhood(center), cube_to_neighborhood(arr)
+            Volume(arr)._get_neighborhood(center), cube_to_neighborhood(arr)
         )
 
         """
@@ -93,82 +50,13 @@ class VolumeTest(unittest.TestCase):
         arr = np.zeros((3, 3, 3), dtype=np.bool_)
         arr[0, 0, 0] = True
         npt.assert_array_equal(
-            Volume(arr).get_neighborhood(center), cube_to_neighborhood(arr)
+            Volume(arr)._get_neighborhood(center), cube_to_neighborhood(arr)
         )
 
     def test_get_candidates(self):
         arr = np.ones((1, 1, 2), dtype=np.bool_)
         v = Volume(arr)
-        cn = v.get_candidates(Axes.Z, Offset.N)
-        cp = v.get_candidates(Axes.Z, Offset.P)
+        cn = v._get_candidates(Axis.Z, Offset.N)
+        cp = v._get_candidates(Axis.Z, Offset.P)
         npt.assert_array_equal(cn, np.array([[[1, 1, 2]]], dtype=np.intp))
         npt.assert_array_equal(cp, np.array([[[1, 1, 1]]], dtype=np.intp))
-
-    def test_process_candidates(self):
-        pass
-
-    @with_log()
-    def test_process_candidates__timing(self):
-        SHAPE = 500
-
-        lut = LookupTable.true()
-        v = Volume.random((SHAPE, SHAPE, SHAPE))
-        rng = np.random.default_rng()
-        COUNTS = 10 ** np.arange(6)
-        for COUNT in COUNTS:
-            candidates = rng.integers(1, SHAPE, (COUNT, 1, 3), dtype=np.intp)
-
-            start = time.perf_counter_ns()
-            _ = v.process_candidates(lut, candidates)
-            stop = time.perf_counter_ns()
-            dur = stop - start
-
-            logging.info(f"{dur}, {dur / COUNT}")
-
-    @with_log()
-    def test_restrict_candidates__timing(self, **kwargs):
-        SHAPE = 500
-
-        lut = LookupTable.true()
-        v = Volume.random((SHAPE, SHAPE, SHAPE))
-        rng = np.random.default_rng()
-        COUNTS = 10 ** np.arange(6)
-        for COUNT in COUNTS:
-            candidates = rng.integers(1, SHAPE, (COUNT, 1, 3), dtype=np.intp)
-
-            start = time.perf_counter_ns()
-            _ = v.restrict_candidates(lut, candidates)
-            stop = time.perf_counter_ns()
-            dur = stop - start
-
-            logging.info(f"{dur}, {dur / COUNT}")
-
-    @with_log()
-    def test_get_candidates__timing(self):
-        SHAPE = 300
-
-        for _ in range(10):
-            v = Volume.random((SHAPE, SHAPE, SHAPE))
-
-            start = time.perf_counter_ns()
-            candidates = v.get_candidates(Axes.X, Offset.N)
-            stop = time.perf_counter_ns()
-            dur = stop - start
-
-            logging.info(f"{dur}, {candidates.shape[0]}")
-
-    @with_log(format="%(message)s")
-    def test_thin__timing(self):
-        SHAPE = (26, 26, 26)
-        RADIUS = max(s // 2 for s in SHAPE)
-
-        lut = LookupTable.read(PurePath("Data/LookupTables/Thinning_MedialSurface.bin"))
-        v = Volume.ball(SHAPE, RADIUS, interior=True)
-        v.write_vtk("ball.vtk")
-        v.thin(lut)
-        v.write_vtk("ball-skel.vtk")
-
-        v = Volume.ball(SHAPE, RADIUS, interior=False)
-        v.write_vtk("antiball.vtk")
-        v.thin(lut)
-        v.write_vtk("antiball-skel.vtk")
